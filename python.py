00f55d87bb3a8020c10a0186b62c7122151e0bf3
Aviral Mittal <aviralmittal0012@gmail.com>
2024-09-15 19:50:07 +0530

    First commit

---
+++ python.py
@@ -0,0 +1,10 @@
+spam_amount =0
+print(spam_amount)
+
+spam_amount = spam_amount +4
+
+if spam_amount > 0:
+  print("bout i dont want any spam")
+
+viking_song = "spam" * spam_amount
+print(viking_song)
